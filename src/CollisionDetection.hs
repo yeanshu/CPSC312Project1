970398befc67591751e3a6c81a3af546301d1158
Yean <m5n2b@ugrad.cs.ubc.ca>
2021-02-18 19:41:16 -0800

    P******* Central

---
+++ src/CollisionDetection.hs
@@ -0,0 +1,40 @@
+module CollisionDetection where
+
+  import GameBoard
+
+  -- | Given position and radius of the ball, return whether a collision occurred on a player
+  paddleCollision :: PongGame -- ^ The game
+                  -> Bool     -- ^ Collision with the paddles?
+
+  paddleCollision game =
+    ((deltaXP1 * deltaXP1 + deltaYP1 * deltaYP1) <
+      (ballRadius * ballRadius)) 
+    where
+
+      -- Ball's center
+      (ballX, ballY) = ballLoc game
+
+      -- Player 1's paddle's center
+      recXP1 = paddlesDistance
+      recYP1 = player1 game
+      
+
+
+      -- Player A's paddle's left bottom corner (needed for collision math)
+      rectCornerXP1 = recXP1 -paddleWidth / 2
+      rectCornerYP1 = recYP1 -paddleHeight / 2
+
+
+
+      deltaXP1 = ballX - max rectCornerXP1 (min ballX (rectCornerXP1 + paddleWidth))
+      deltaYP1 = ballY - max rectCornerYP1 (min ballY (rectCornerYP1 + paddleHeight))
+
+  -- | Given position and radius of the ball, return whether a collision occurred on the wall.
+  wallCollision :: Position -- ^ Ball's position
+                -> Radius   -- ^ Ball's radius
+                -> Bool     -- ^ Collision with the walls?
+  wallCollision (_, y) radius =
+      topCollision || bottomCollision
+    where
+      topCollision = y + radius >= fromIntegral width / 2
+      bottomCollision = y - radius <= -(fromIntegral width / 2)
---
+++ src/GameBoard.hs
@@ -0,0 +1,47 @@
+module GameBoard where
+
+  paddleWidth, paddleHeight, paddleBorder, paddlesDistance, paddleStep, ballRadius, brickwidth, brickheight  :: Float
+  paddleWidth = 86
+  paddleHeight = 26
+  paddleBorder = 1
+  paddlesDistance = 0
+  paddleStep = 5
+  ballRadius = 10
+  brickheight = 10
+  brickwidth = 50
+
+  width, height, offset:: Int
+  width = 1000
+  height = 1000
+  offset = 100
+
+
+  type Radius = Float
+  type Position = (Float, Float)
+
+  -- | The game state
+  data GameState =
+    Playing | Paused
+    deriving Show
+
+  -- | A data structure to hold the state of the Pong game.
+  data PongGame = Game
+    { gameState :: GameState
+    , ballLoc :: (Float, Float) -- ^ Pong ball (x, y) location.
+    , ballVel :: (Float, Float) -- ^ Pong ball (x, y) velocity.
+    , player1 :: Float          -- ^ Left player paddle height.
+                                -- Zero is the middle of the screen.
+    , player1v :: Float   -- ^ player1's paddle's velocity.
+    , paused :: Bool            -- ^ if the game is paused
+    } deriving Show
+
+  -- | Initialize the game with this game state.
+  initialState :: PongGame
+  initialState = Game
+    { gameState = Playing
+    , ballLoc = (0, 0)
+    , ballVel = (40, -140)
+    , player1 = 40
+    , player1v = 0
+    , paused  = False
+    }
